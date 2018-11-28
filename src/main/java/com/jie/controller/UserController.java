61104ddbf62f3102da6ffa00229163326e74e72a
857910915 <857910915@qq.com>
2018-11-28 15:54:06 +0800

    用户注册登录完成版1

---
+++ src/main/java/com/jie/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.jie.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.jie.domain.User;
+import com.jie.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @RequestMapping("/toLogin")
+    @ResponseBody
+    public String toLogin(String username, String password, HttpServletRequest request) {
+        List list = userService.toLogin(username, password,request);
+        System.out.println("");
+        return JSON.toJSONString(list);
+
+    }
+
+    @RequestMapping("/toRegister")
+    @ResponseBody
+    public String toRegister(User user) {
+
+        String msg=userService.toRegister(user);
+        return JSON.toJSONString(msg);
+    }
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
